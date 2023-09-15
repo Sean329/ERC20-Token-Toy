19c0c6554359bcbac626cefed491f7f441d5cbd6
Xiangyu Xu <xuxiangy@amazon.com>
2023-09-15 12:34:17 -0700

    bug fix

--- scripts/airdrop.js
+++ scripts/airdrop.js
@@ -4,8 +4,10 @@ require("dotenv").config();
 async function main() {
     const evieAddress = "0x087aF2906D4cFa2E0c01910854815aA945F9B757";
     const evieContract = await hre.ethers.getContractAt("Evie", evieAddress);
-
-    await evieContract.transfer(process.env.ADDRESS_AIRDROP_TO, 10000);
+    
+    // @dev js numbers do not have enough precision so borrowing the parseEther method to make it BigNumber
+    const amount = hre.ethers.parseEther("10000");
+    await evieContract.transfer(process.env.ADDRESS_AIRDROP_TO, amount);
 
     console.log(
         `Airdropped to ${process.env.ADDRESS_AIRDROP_TO}`
