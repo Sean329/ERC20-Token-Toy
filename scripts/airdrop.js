bfad04fa064d0ccf1a84f14e2163799b7629e833
Xiangyu Xu <xuxiangy@amazon.com>
2023-09-16 13:56:35 -0700

    update BigNumber

--- scripts/airdrop.js
+++ scripts/airdrop.js
@@ -5,8 +5,11 @@ async function main() {
     const evieAddress = "0x087aF2906D4cFa2E0c01910854815aA945F9B757";
     const evieContract = await hre.ethers.getContractAt("Evie", evieAddress);
     
-    // @dev js numbers do not have enough precision so borrowing the parseEther method to make it BigNumber
-    const amount = hre.ethers.parseEther("10000");
+    // @dev js numbers do not have enough precision so make it BigNumber;
+    // or just borrow the parseEther method if the decimal is 18 same as ETH
+    
+    // const amount = hre.ethers.parseEther("10000");
+    const amount =BigInt(100*(10**18));
     await evieContract.transfer(process.env.ADDRESS_AIRDROP_TO, amount);
 
     console.log(
